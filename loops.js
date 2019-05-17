7b12b64e2ac50a86f09aae1ca2dd94211692e3c2
jessica cats <jesstcats@gmail.com>
2019-05-17 19:16:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,45 +1,30 @@
-/*for (var x = 1; x < 50; x++) {
-  console.log("hello world");
-  x;
-  console.log(x);
-}*/
-
-//var array = [];
-
-/*function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if(i === 1) {
-      array.push("I am 1 strange loop.");
-    }
-    else {
-      array.push(`I am ${i} strange loops.`);
-    }
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
   return array;
 }
-
-forLoop([]);
 
 function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n);
+  let countdown = n;
+
+    while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return "done";
   }
-  return "done";
-}
-
-whileLoop(10);*/
-
-function maybeTrue() {
-  return Math.random() >= 0.5;
-}
-
-var array = [1,2,3];
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
   do {
     array.pop();
-  } while (array.length > 0); //&& maybeTrue());
-  return array;
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
-
-doWhileLoop(array);
