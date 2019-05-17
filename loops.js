4ee55dd45bc789b86a0a8e8c710d1ecc20866cc0
jessica cats <jesstcats@gmail.com>
2019-05-17 19:25:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,11 @@
-function doWhileLoop(array){
-function maybeTrue() {
-  return Math.random() >= 0.5;
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    }
+    else {
+      array.push("I am ${i} strange loops.");
+    }
+  }
+  return array;
 }
-do{
-  array.pop();
-}
-while (array.length > 0 && maybeTrue())
-return array ;
-}
-
-doWhileLoop([1,2,3,4,5])
